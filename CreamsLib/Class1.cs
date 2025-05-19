4facf5befbb90ac064ed962914f91fda0539d7b2
백수영 <tmdghtndud@gmail.com>
2025-05-19 11:10:25 +0900

    first

---
+++ CreamsLib/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace CreamsLib
+{
+    public class Class1
+    {
+        public int DoSomething(int a , int b)
+        {
+            int rv = a + b;
+
+            return rv;
+        }
+
+        public void DoError()
+        {
+            throw new Exception("Error in CreamsLib");
+        }
+    }
+}
